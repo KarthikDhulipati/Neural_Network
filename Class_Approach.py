781821a58bb7dc95ce9cb6ffde433b7320e2bf11
Karthik Dhulipati <112795118+KarthikDhulipati@users.noreply.github.com>
2025-01-01 23:03:42 +0530

    Created the class for Softmax activation function and added to the Class_Approach.py file. Now, the layers can use the softmax function in order to propagate the data forward.

--- Class_Approach.py
+++ Class_Approach.py
@@ -22,11 +22,11 @@ class Activation_ReLu:
         self.output = np.maximum(0, inputs)
 
 
-X = [[1, 2, 3, 2.5],  # Sample Input Data
-     [2.0, 5.0, 1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
+class Activation_SoftMax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
 '''
 This current approach requires us to code the entire network in 2 phases.
@@ -59,11 +59,17 @@ To be added Layers
     - Dropout Layer
 
 '''
-layer1 = Layer_Dense(2, 5)
+
+X, y = spiral_data(100, 3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLu()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_SoftMax()
 
-layer1.forward(X)
-print("Layer1 output = ", layer1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-activation1.forward(layer1.output)
-print("\nActivation1 layer1 output = ", activation1.output)
+print("Activation2.output = ", activation2.output)
