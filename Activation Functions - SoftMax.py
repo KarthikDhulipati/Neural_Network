3b16c64fe10ce193004a2fb56a34c5e002ae64da
Karthik Dhulipati <112795118+KarthikDhulipati@users.noreply.github.com>
2025-01-01 23:03:36 +0530

    Created the class for Softmax activation function and added to the Class_Approach.py file. Now, the layers can use the softmax function in order to propagate the data forward.

--- Activation Functions - SoftMax.py
+++ Activation Functions - SoftMax.py
@@ -1,27 +1,50 @@
 import math
-import numpy
+import numpy as np
+import nnfs
+'''
+Here's the working of the SoftMax Function:
 
-layer_outputs = [4.8, 1.21, 2.385]
+    [ 1 ]       [ e^1 ]     [ (e^1)/(e^1+e^2+e^3) ]     [ 0.09 ]
+    | 2 |   =   | e^2 | =   | (e^2)/(e^1+e^2+e^3) | =   | 0.24 |
+    [ 3 ]       [ e^3 ]     [ (e^3)/(e^1+e^2+e^3) ]     [ 0.67 ]
+    
+    S_(i,j)  =  e^(z_(i,j) ) / (∑_(l=1)^L▒e^(Z_(i,j) ) )
+'''
 
-E = math.e
-# E = 2.71828182846
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
 
-exp_values = [] # Exponentiated values, i.e., e^x
+# E = math.e
+# # E = 2.71828182846
+#
+# exp_values = [] # Exponentiated values, i.e., e^x
+#
+# for output in layer_outputs:
+#     exp_values.append(E**output)
 
-for output in layer_outputs:
-    exp_values.append(E**output)
-
-print("Exponentiated values : ", exp_values)
+# print("Exponentiated values : ", exp_values)
 
 '''Now, we normalize the values in the newly created array - 'exp_values'
 
 # Normalization process:'''
 
-norm_base = sum(exp_values)
-norm_values = []
+# norm_base = sum(exp_values)
+# norm_values = []
+#
+# for value in exp_values:
+#     norm_values.append(value/norm_base)
 
-for value in exp_values:
-    norm_values.append(value/norm_base)
+# print("Normalized Values : ", norm_values)
+# print("Sum of Normalized Values : ", sum(norm_values))
 
-print("Normalized Values : ", norm_values)
-print("Sum of Normalized Values : ", sum(norm_values))
+
+'''Another Approach using numpy: '''
+exp_values = np.exp(layer_outputs)
+
+print("Exp_values: ", np.sum(layer_outputs, axis=1, keepdims=True))
+
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+print(norm_values)
+
